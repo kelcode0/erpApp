fe17aa6add60c8bf58b7e895cac6ab1b27e0cfc4
Kelvin Ortiz <97717292+kelcode0@users.noreply.github.com>
2024-06-23 13:29:30 -0400

    Update environment.ts
    a

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAUcG5K-8MDGgHS4Gc4j9U9L1OoYeT2GQI',
-    authDomain: 'erp16-de28f.firebaseapp.com',
-    projectId: 'erp16-de28f',
-    storageBucket: 'erp16-de28f.appspot.com',
-    messagingSenderId: '112287852441',
-    appId: '1:112287852441:web:f6598cc35b7c3ae425d508',
-    measurementId: 'G-SW1XC0M6G4',
-  },
+
 };
